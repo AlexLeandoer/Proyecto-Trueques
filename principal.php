e73e97a645a2ca5a604cbeca8b0c79f6fe0d3de7
AlexLeandoer <alconpla@gmail.com>
2019-04-27 17:45:53 +0200

    seccion destacados, sass + css arreglados

--- principal.php
+++ principal.php
@@ -1,48 +1,51 @@
 <?php include_once("header.php"); ?>
-  <header id="header2">
-    <div class="logo">
-        logo<!-- <img src="img/logo.png" alt=""> -->
+<header id="header2">
+<div class="logo">
+    logo<!-- <img src="img/logo.png" alt=""> -->
+</div>
+<form class="form-inline d-flex justify-content-center buscador">
+    <div class="icon-search">
+        <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
     </div>
-    <form class="form-inline d-flex justify-content-center buscador">
-        <div class="icon-search">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        </div>
-    </form>
+</form>
 
-    <div class="botones">
-        <a href="#">Login</a>
-        <a href="#">Carrito</a>
+<div class="botones">
+    <a href="#">Login</a>
+    <a href="#">Carrito</a>
+</div>
+</header>
+
+<section id="categorias">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3" id="nav-left">
+                <p>Todas las categorías</p>
+                <ul class="list-unstyled">
+                    <li><img href="icons/pc.svg"><a href="#">Informática</a></li>
+                    <li><a href="#">Electrónica</a></li>
+                    <li><a href="#">Telefonía</a></li>
+                    <li><a href="#">Propuestas</a></li>
+                    <li><a href="#">Cocina</a></li>
+                    <li><a href="#">Diseño</a></li>
+                    <li><a href="#">Eventos</a></li>
+                    <li><a href="#">Otros</a></li>
+                </ul>
+            </div>         
+            <div class="col-md-9" id="contenido">
+        
+            </div>
+        </div> 
+        <!-- fin row -->
     </div>
-  </header>
+    <!-- fin container -->
+</section>
+<div class="col-md-4" id="navegacion">
+    <a href="#" id="home">Home</a>
+    <a href="#" id="destacados">Destacados</a>
+</div>
 
-  <section id="categorias">
-      <div class="container">
-          <div class="row">
-              <div class="col-md-3 ">
-                  <p>Todas las categorías</p>
-                  <ul class="list-unstyled">
-                      <li><a href="#">Informática</a></li>
-                      <li><a href="#">Electrónica</a></li>
-                      <li><a href="#">Telefonía</a></li>
-                      <li><a href="#">Propuestas</a></li>
-                      <li><a href="#">Cocina</a></li>
-                      <li><a href="#">Diseño</a></li>
-                      <li><a href="#">Eventos</a></li>
-                      <li><a href="#">Otros</a></li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-  </section>
+    
 
-  <section class="container">
-      <div class="col-md-4" id="navegacion">
-        <a href="#" id="home">Home</a>
-        <a href="#" id="destacados">Destacados</a>
-      </div>
-      <div class="col-md-8" id="contenido">
-      </div>
-  </section>
 
 <?php include_once("footer.php"); ?>
 <script>
@@ -53,8 +56,7 @@
         });
         $("#destacados").click(function(e){
             e.preventDefault();
-            $('#contenido').load("destacados.html");
+            $('#contenido').load("d.html");
         });
     });
-    
 </script>
