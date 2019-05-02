ce6579605bc789a0b42f4d9545768b7b1accd72f
AlexLeandoer <alconpla@gmail.com>
2019-05-02 13:20:24 +0200

    muchos cambios + inicio de perfil.php

---
+++ perfil.php
@@ -0,0 +1,58 @@
+<?php include_once("header.php"); ?>
+<header id="header2">
+    <div class="logo">
+        logo<!-- <img src="img/logo.png" alt=""> -->
+    </div>
+    <form class="form-inline d-flex justify-content-center buscador">
+        <div class="icon-search">
+            <input class="form-control mr-sm-2" type="search" placeholder="Buscar..." aria-label="Search">
+        </div>
+    </form>
+
+    <div class="botones">
+        <a href="#">Login</a>
+        <a href="#">Carrito</a>
+    </div>
+</header>
+
+<section id="perfil">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="menu">
+                    <h4>Perfil</h4>
+                    <div class="enlaces">
+                        <a href="#">Mis datos</a>
+                        <a href="#">Opiniones</a>
+                        <a href="#">Mensajes</a>
+                        <a href="#">Pedidos</a>
+                        <a href="#">Ofertas</a>
+                    </div>
+                    <h4>Servicios</h4>
+                    <div class="enlaces">
+                        <a href="#">Nuevo servicio</a>
+                        <a href="#">Editar servicios</a>
+                        <a href="#">Lorem ipsum</a>
+                        <a href="#">Lorem ipsum</a>
+                    </div>
+                    <h4>Ayuda</h4>
+                    <div class="enlaces">
+                        <a href="#">Lorem ipsum</a>
+                        <a href="#">Lorem ipsum</a>
+                        <a href="#">Lorem ipsum</a>
+                        <a href="#">Lorem ipsum</a>
+                        <a href="#">Lorem ipsum</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <h3>Mi cuenta</h3>
+                <div class="profile">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include_once("footer.php"); ?>
--- principal.php
+++ principal.php
@@ -18,17 +18,17 @@
 <section id="categorias">
     <div class="container">
         <div class="row">
-            <div class="col-md-3" id="nav-left">
+            <div class="col-md-3 col-sm-10" id="nav-left">
                 <p>Todas las categorías</p>
                 <ul class="list-unstyled">
-                    <li><img href="icons/pc.svg"><a href="#">Informática</a></li>
-                    <li><a href="#">Electrónica</a></li>
-                    <li><a href="#">Telefonía</a></li>
-                    <li><a href="#">Propuestas</a></li>
-                    <li><a href="#">Cocina</a></li>
-                    <li><a href="#">Diseño</a></li>
-                    <li><a href="#">Eventos</a></li>
-                    <li><a href="#">Otros</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Informática</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Electrónica</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Telefonía</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Propuestas</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Cocina</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Diseño</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Eventos</a></li>
+                    <li><img src="http://placehold.it/30x30" alt=""><a href="#">Otros</a></li>
                 </ul>
             </div>         
             <div class="col-md-9" id="contenido">
@@ -44,8 +44,6 @@
     <a href="#" id="d">Destacados</a>
 </div>
 
-    
-
 
 <?php include_once("footer.php"); ?>
 <script>
@@ -56,7 +54,7 @@
         });
         $("#d").click(function(e){
             e.preventDefault();
-            $('#contenido').load("d.html");
+            $('#contenido').load("destacados.php");
         });
     });
 </script>
